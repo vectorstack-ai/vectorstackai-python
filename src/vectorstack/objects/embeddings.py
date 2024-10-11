5a604972f51381d17b0ebeaed364fa73bbafc49a
shreyas <shreyas.saxena2@gmail.com>
2024-10-11 14:49:40 +0000

    added repr to Embedding objects

--- src/vectorstack/objects/embeddings.py
+++ src/vectorstack/objects/embeddings.py
@@ -17,10 +17,12 @@ class EmbeddingsObject(BaseObject):
             self.embeddings = response.json()['output']['embeddings']
         
     def __str__(self) -> str:
-        # Display number of embeddings, and length of each embedding
         if self.embeddings:
             num_embeddings = len(self.embeddings)
             embedding_dims = len(self.embeddings[0])
             return f"EmbeddingsObject(num_embeddings={num_embeddings}, embedding_dims={embedding_dims})"
         else:
             return "Error: EmbeddingsObject(no embeddings returned)"
+        
+    def __repr__(self) -> str:
+        return self.__str__()   
