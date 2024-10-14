e92c751913164c434912cce0e8452b9a666f3be9
Shreyas Saxena <shreyas.saxena24@gmail.com>
2024-10-14 11:52:38 +0200

    added support to handle bad gateway error

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="vectorstack",
-    version="0.1",
+    version="0.4",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "requests",
-        "tenacity",
+        "requests>=2.32.3",
+        "tenacity==9.0.0",
     ],
+    license="MIT", 
     author="Shreyas Saxena",
     author_email="shreyas@vectorstack.ai",
     description="VectorStack AI's Official Python Library",
-    python_requires='>=3.7',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown", 
+    python_requires='>=3.8',
 )
