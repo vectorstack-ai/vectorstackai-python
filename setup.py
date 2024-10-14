052ce5014d8fc94a91bac18e2a763911328fd3e3
Shreyas Saxena <shreyas.saxena24@gmail.com>
2024-10-14 11:52:56 +0200

    scripting changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vectorstack",
-    version="0.4",
+    version="0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
