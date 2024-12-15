362351b28a0ac90c8ad5ec0d1e4c4708b4db2f33
shreyassaxena <shreyas.saxena2@gmail.com>
2024-12-15 05:00:05 +0000

    polished up embedding api resource and added handling of exceptions/errors raised from the backend server

--- src/vectorstack/api_resources/embeddings.py
+++ src/vectorstack/api_resources/embeddings.py
@@ -1,7 +1,8 @@
 import requests
 import ipdb
 from vectorstack.objects import EmbeddingsObject
-
+from vectorstack import error
+from vectorstack.utils import raise_error_from_response
 
 class BaseAPIResource(object):
     DEFAULT_TIMEOUT = 300
@@ -21,12 +22,12 @@ class Embedding(BaseAPIResource):
     CLASS_URL = "https://api.vectorstack.ai/embeddings"
 
     @classmethod
-    def encode(cls, input, model, languages, is_query, instruction, **kwargs):
+    def encode(cls, texts, model, languages, is_query, instruction, **kwargs):
         """
         Creates a new embedding for the provided input and parameters.
 
         Args:
-            input (list): A list of strings to be embedded.
+            texts(list): A list of strings to be embedded.
             model (str): The name of the model to use for embedding.
             languages (list): A list of languages corresponding to each input string.
             is_query (bool): Whether the input is a query or not.
@@ -37,21 +38,27 @@ class Embedding(BaseAPIResource):
             EmbeddingsObject: An object containing the generated embeddings and related information.
 
         Raises:
-            requests.RequestException: If there's an error in the API request.
+            VectorStackError: An appropriate subclass of VectorStackError based on the error type.
         """
         
         json_data= {
-            'model': model,
-            'sentences': input,
-            'languages': languages,
-            'is_query': is_query,
-            'instruction': instruction
+            'input': {
+                'model': model,
+                'texts': texts,
+                'languages': languages,
+                'is_query': is_query,
+                'instruction': instruction,
+            },
+            'api_key': kwargs.get("api_key")
         } 
-        ipdb.set_trace()
         response = requests.post(cls.CLASS_URL, 
                                  headers=cls.HEADERS, 
                                  json=json_data, 
-                                 api_key=kwargs.get("api_key"),
                                  timeout=kwargs.get("timeout", cls.DEFAULT_TIMEOUT)) 
-
-        return EmbeddingsObject(response)
+        
+        if response.status_code != 200:
+            raise_error_from_response(response)
+            
+        return response
+    
+    
--- src/vectorstack/utils.py
+++ src/vectorstack/utils.py
@@ -1,7 +1,7 @@
 import os
 
 import vectorstack
-
+from vectorstack import error
 
 def get_api_key() -> str:
     api_key = vectorstack.api_key or os.environ.get("VECTORSTACK_API_KEY")
@@ -13,3 +13,51 @@ def get_api_key() -> str:
             "No API key provided. You can set your API key in code using 'vectorstack.api_key = <API-KEY>', "
             "or set the environment variable VECTORSTACK_API_KEY=<API-KEY>. "
             "Visit https://www.vectorstack.ai to sign up for a free API key.")
+        
+        
+        
+def raise_error_from_response(response):
+    """
+    Raise an appropriate exception based on the error response from the API.
+
+    This function dynamically maps error types to their corresponding exception classes,
+    extracts error information from the response, and raises the appropriate exception.
+
+    Args:
+        response (requests.Response): The API response object containing error information.
+
+    Raises:
+        VectorStackError: An appropriate subclass of VectorStackError based on the error type.
+
+    Note:
+        This function assumes that the error response is in JSON format and follows
+        a specific structure with an 'error' key containing error details.
+    """
+    # Dynamically create mapping of error types to exception classes
+    error_class_mapping = {
+        name: getattr(error, name)
+        for name in dir(error)
+        if isinstance(getattr(error, name), type) and issubclass(getattr(error, name), error.VectorStackError)
+    }
+    
+    # Get the error data from the response
+    error_data = response.json().get('error', {})
+    message = error_data.get('message')
+    http_status = error_data.get('http_status')
+    code = error_data.get('code')
+    http_body = error_data.get('http_body')
+    json_body = error_data.get('json_body')
+    headers = response.headers
+
+    # Get the corresponding exception class based on the error type
+    exception_class = error_class_mapping.get(error_data.get('type'), error.VectorStackError)
+
+    # Raise the exception with the appropriate data
+    raise exception_class(
+        message=message,
+        http_body=http_body,
+        http_status=http_status,
+        json_body=json_body,
+        headers=headers,
+        code=code,
+    )
