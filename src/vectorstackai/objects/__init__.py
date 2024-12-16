f72a676e60316cfd7eaf652ad050bf001432df48
shreyassaxena <shreyas.saxena2@gmail.com>
2024-12-16 17:16:25 +0000

    fixed the import of PointObj in __init__.py

--- src/vectorstackai/objects/__init__.py
+++ src/vectorstackai/objects/__init__.py
@@ -1,3 +1,3 @@
 from vectorstackai.objects.embeddings import EmbeddingsObject
 from vectorstackai.objects.index import IndexObject
-from vectorstackai.objects.points import PointObj
+from vectorstackai.objects.points import PointObject
